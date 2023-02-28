386a2893071e02e1612b7fcddf9f346981e24af4
Ifham <ifhamhusain778866@gmail.com>
2023-02-28 20:08:24 +0530

    minor changes part2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import "./App.css";
 import { useState, useEffect } from "react";
 import { NumericFormat } from "react-number-format";
@@ -198,7 +176,6 @@ function App() {
         </div>
       </div>
     </div>
->>>>>>> release
     </div>
   );
 }
